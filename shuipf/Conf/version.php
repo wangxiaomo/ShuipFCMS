81e296cdf5da112e94359a662c6fc3fe316a2bed
shuipf <admin@abc3210.com>
2013-06-24 17:18:32 +0800

    更改版本号

--- shuipf/Conf/version.php
+++ shuipf/Conf/version.php
@@ -6,6 +6,6 @@
  */
 return array(
     "SHUIPF_APPNAME" => "ShuipFCMS_UTF8_Free", //产品名称
-    "SHUIPF_BUILD" => 20130517, //产品流水号
+    "SHUIPF_BUILD" => 20130624, //产品流水号
     "SHUIPF_VERSION" => "1.0.0", //产品版本号
 );
