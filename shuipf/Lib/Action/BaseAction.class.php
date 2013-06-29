2d867f7d1bfe0ee232689fb36a2fd4efcdd740ed
水平凡 <admin@abc3210.com>
2013-06-29 15:44:38 +0800

    修改page方法

--- shuipf/Lib/Action/BaseAction.class.php
+++ shuipf/Lib/Action/BaseAction.class.php
@@ -85,26 +85,26 @@ class BaseAction extends AppframeAction {
         return $status;
     }
 
-    /**
-     * 分页
-     * @param type $Total_Size
-     * @param type $Page_Size
-     * @param type $Current_Page
-     * @param type $listRows
-     * @param type $PageParam
-     * @param type $PageLink
-     * @param type $Static
-     * @return \Page 
+     /**
+     * 分页输出
+     * @staticvar array $_pageCache
+     * @param type $Total_Size 信息总数
+     * @param type $Page_Size 每页显示信息数量
+     * @param type $Current_Page 当前分页号
+     * @param type $List_Page 每次显示几个分页导航链接
+     * @param type $PageParam 接收分页号参数的标识符
+     * @param type $PageLink 分页规则 
+     *                          array(
+                                    "index"=>"http://www.abc3210.com/192.html",//这种是表示当前是首页，无需加分页1
+                                    "list"=>"http://www.abc3210.com/192-{page}.html",//这种表示分页非首页时启用
+                                 )
+     * @param type $static 是否开启静态
+     * @param string $TP 模板
+     * @param array $Tp_Config 模板配置
+     * @return array|\Page
      */
-    protected function page($Total_Size = 1, $Page_Size = 0, $Current_Page = 1, $listRows = 6, $PageParam = '', $PageLink = '', $Static = FALSE) {
-        import('Page');
-        if ($Page_Size == 0) {
-            $Page_Size = C("PAGE_LISTROWS");
-        }
-        if (empty($PageParam)) {
-            $PageParam = C("VAR_PAGE");
-        }
-        $Page = new Page($Total_Size, $Page_Size, $Current_Page, $listRows, $PageParam, $PageLink, $Static);
+    protected function page($Total_Size = 1, $Page_Size = 0, $Current_Page = 0, $List_Page = 6, $PageParam = '', $PageLink = '', $static = FALSE, $TP = "", $Tp_Config = "") {
+        $Page = page($Total_Size, $Page_Size, $Current_Page, $List_Page, $PageParam, $PageLink, $static, $TP, $Tp_Config);
         $Page->SetPager('Admin', '共有{recordcount}条信息&nbsp;{pageindex}/{pagecount}&nbsp;{first}{prev}&nbsp;{liststart}{list}{listend}&nbsp;{next}{last}&nbsp;转到{jump}页', array("listlong" => "6", "first" => "首页", "last" => "尾页", "prev" => "上一页", "next" => "下一页", "list" => "*", "disabledclass" => ""));
         return $Page;
     }
