9091576718c7591952abd861169dc650c4d359d8
水平凡 <admin@abc3210.com>
2013-07-29 20:04:22 +0800

    更新str_cut函数

--- shuipf/Common/common.php
+++ shuipf/Common/common.php
@@ -294,75 +294,41 @@ function genRandomString($len = 6) {
 }
 
 /**
- * 字符截取 支持UTF8/GBK
- * @param $string
- * @param $length
+ * 字符截取
+ * @param $string 需要截取的字符串
+ * @param $length 长度
  * @param $dot
  */
-function str_cut($string, $length, $dot = '...') {
-    $strlen = strlen($string);
-    if ($strlen <= $length)
-        return $string;
-    $string = str_replace(array(' ', '&nbsp;', '&amp;', '&quot;', '&#039;', '&ldquo;', '&rdquo;', '&mdash;', '&lt;', '&gt;', '&middot;', '&hellip;'), array('∵', ' ', '&', '"', "'", '“', '”', '—', '<', '>', '·', '…'), $string);
-    $strcut = '';
-    if (strtolower(C("DEFAULT_CHARSET")) == 'utf-8') {
-        $length = intval($length - strlen($dot) - $length / 3);
-        $n = $tn = $noc = 0;
-        while ($n < strlen($string)) {
-            $t = ord($string[$n]);
-            if ($t == 9 || $t == 10 || (32 <= $t && $t <= 126)) {
-                $tn = 1;
-                $n++;
-                $noc++;
-            } elseif (194 <= $t && $t <= 223) {
-                $tn = 2;
-                $n += 2;
-                $noc += 2;
-            } elseif (224 <= $t && $t <= 239) {
-                $tn = 3;
-                $n += 3;
-                $noc += 2;
-            } elseif (240 <= $t && $t <= 247) {
-                $tn = 4;
-                $n += 4;
-                $noc += 2;
-            } elseif (248 <= $t && $t <= 251) {
-                $tn = 5;
-                $n += 5;
-                $noc += 2;
-            } elseif ($t == 252 || $t == 253) {
-                $tn = 6;
-                $n += 6;
-                $noc += 2;
-            } else {
-                $n++;
-            }
-            if ($noc >= $length) {
-                break;
-            }
-        }
-        if ($noc > $length) {
-            $n -= $tn;
-        }
-        $strcut = substr($string, 0, $n);
-        $strcut = str_replace(array('∵', '&', '"', "'", '“', '”', '—', '<', '>', '·', '…'), array(' ', '&amp;', '&quot;', '&#039;', '&ldquo;', '&rdquo;', '&mdash;', '&lt;', '&gt;', '&middot;', '&hellip;'), $strcut);
-    } else {
-        $dotlen = strlen($dot);
-        $maxi = $length - $dotlen - 1;
-        $current_str = '';
-        $search_arr = array('&', ' ', '"', "'", '“', '”', '—', '<', '>', '·', '…', '∵');
-        $replace_arr = array('&amp;', '&nbsp;', '&quot;', '&#039;', '&ldquo;', '&rdquo;', '&mdash;', '&lt;', '&gt;', '&middot;', '&hellip;', ' ');
-        $search_flip = array_flip($search_arr);
-        for ($i = 0; $i < $maxi; $i++) {
-            $current_str = ord($string[$i]) > 127 ? $string[$i] . $string[++$i] : $string[$i];
-            if (in_array($current_str, $search_arr)) {
-                $key = $search_flip[$current_str];
-                $current_str = str_replace($search_arr[$key], $replace_arr[$key], $current_str);
-            }
-            $strcut .= $current_str;
+function str_cut($sourcestr, $length, $dot = '...') {
+    $returnstr = '';
+    $i = 0;
+    $n = 0;
+    $str_length = strlen($sourcestr); //字符串的字节数 
+    while (($n < $length) && ($i <= $str_length)) {
+        $temp_str = substr($sourcestr, $i, 1);
+        $ascnum = Ord($temp_str); //得到字符串中第$i位字符的ascii码 
+        if ($ascnum >= 224) {//如果ASCII位高与224，
+            $returnstr = $returnstr . substr($sourcestr, $i, 3); //根据UTF-8编码规范，将3个连续的字符计为单个字符         
+            $i = $i + 3; //实际Byte计为3
+            $n++; //字串长度计1
+        } elseif ($ascnum >= 192) { //如果ASCII位高与192，
+            $returnstr = $returnstr . substr($sourcestr, $i, 2); //根据UTF-8编码规范，将2个连续的字符计为单个字符 
+            $i = $i + 2; //实际Byte计为2
+            $n++; //字串长度计1
+        } elseif ($ascnum >= 65 && $ascnum <= 90) { //如果是大写字母，
+            $returnstr = $returnstr . substr($sourcestr, $i, 1);
+            $i = $i + 1; //实际的Byte数仍计1个
+            $n++; //但考虑整体美观，大写字母计成一个高位字符
+        } else {//其他情况下，包括小写字母和半角标点符号，
+            $returnstr = $returnstr . substr($sourcestr, $i, 1);
+            $i = $i + 1;            //实际的Byte数计1个
+            $n = $n + 0.5;        //小写字母和半角标点等与半个高位字符宽...
         }
     }
-    return $strcut . $dot;
+    if ($str_length > $length) {
+        $returnstr = $returnstr . $dot; //超过长度时在尾处加上省略号
+    }
+    return $returnstr;
 }
 
 /**
