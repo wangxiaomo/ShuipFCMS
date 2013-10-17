a8c057df7270c797ffd3af84f356cedc7a88e91c
shuipf <admin@abc3210.com>
2013-10-17 14:58:08 +0800

    增加模块图标

--- shuipf/Modules/Collection/Install/Config.inc.php
+++ shuipf/Modules/Collection/Install/Config.inc.php
@@ -4,18 +4,13 @@
  * Some rights reserved：abc3210.com
  * Contact email:admin@abc3210.com
  */
-//模块目录
-$module = 'Collection';
-//模块名称
-$modulename = '采集';
-//模块简介
-$introduce = '采集模块是可以批量采集目标网站内容入库！';
-//模块作者
-$author = '水平凡';
-//作者地址
-$authorsite = 'http://www.abc3210.com/';
-//作者邮箱
-$authoremail = 'admin@abc3210.com';
-//版本号
-$version = "20130505";
-?>
+return array(
+    'module' => 'Collection',
+    'modulename' => '采集',
+    'introduce' => '采集模块是可以批量采集目标网站内容入库！',
+    'author' => '水平凡',
+    'authorsite' => 'http://www.shuipfcms.com/',
+    'authoremail' => 'admin@abc3210.com',
+    'version' => '20130505',
+    'adaptation' => '1.1.0',
+);
