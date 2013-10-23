c2a84f366ded338899a29975da557f373e7b5d31
shuipf <admin@abc3210.com>
2013-10-23 15:22:19 +0800

    加上友情链接模块！

---
+++ shuipf/Modules/Links/Install/Config.inc.php
@@ -0,0 +1,16 @@
+<?php 
+/**
+ * 模块安装，基本配置
+ * Some rights reserved：abc3210.com
+ * Contact email:admin@abc3210.com
+ */
+return array(
+    'module' => 'Links',
+    'modulename' => '友情链接',
+    'introduce' => '友情链接模块！',
+    'author' => '水平凡',
+    'authorsite' => 'http://www.shuipfcms.com/',
+    'authoremail' => 'admin@abc3210.com',
+    'version' => '1.0',
+    'adaptation' => '1.1.0',
+);
