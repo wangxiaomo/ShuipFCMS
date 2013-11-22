3028d4f10bbdbbe3b6493d8d8404c134e04a2ae6
水平凡 <admin@abc3210.com>
2013-11-22 13:06:57 +0800

    修改流水号！

--- shuipf/Conf/version.php
+++ shuipf/Conf/version.php
@@ -6,6 +6,6 @@
  */
 return array(
     "SHUIPF_APPNAME" => "ShuipFCMS_UTF8_Free", //产品名称
-    "SHUIPF_BUILD" => 20131103, //产品流水号
+    "SHUIPF_BUILD" => 20131122, //产品流水号
     "SHUIPF_VERSION" => "1.2.0", //产品版本号
 );
