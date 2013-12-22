f32df9e67a4f50315e06c79a5cc8fa21103ffa9f
水平凡 <admin@abc3210.com>
2013-12-22 22:01:07 +0800

    评论js自动判断加载ajaxForm.js

--- statics/js/comment/embed.js
+++ statics/js/comment/embed.js
@@ -137,6 +137,59 @@ function getCursortPosition (ctrl) {
                 var t = n.createElement("link");
                 t.type = "text/css", t.rel = "stylesheet", t.href = e, u.appendChild(t)
             },
+			//加载js
+			loadJS:function(id,url){
+				var  xmlHttp = null;
+				if(window.ActiveXObject)//IE
+				{
+					try {
+						//IE6以及以后版本中可以使用
+						xmlHttp = new ActiveXObject("Msxml2.XMLHTTP");
+					}
+					catch (e) {
+						//IE5.5以及以后版本可以使用
+						xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+					}
+				}
+				else if(window.XMLHttpRequest)//Firefox，Opera 8.0+，Safari，Chrome
+				{
+					xmlHttp = new XMLHttpRequest();
+				}
+				//采用同步加载
+				xmlHttp.open("GET",url,false);
+				//发送同步请求，如果浏览器为Chrome或Opera，必须发布后才能运行，不然会报错
+				xmlHttp.send(null);
+				//4代表数据发送完毕
+				if ( xmlHttp.readyState == 4 )
+				{
+					//0为访问的本地，200到300代表访问服务器成功，304代表没做修改访问的是缓存
+					if((xmlHttp.status >= 200 && xmlHttp.status <300) || xmlHttp.status == 0 || xmlHttp.status == 304)
+					{
+						var myHead = document.getElementsByTagName("HEAD").item(0);
+						var myScript = document.createElement( "script" );
+						myScript.language = "javascript";
+						myScript.type = "text/javascript";
+						myScript.id = id;
+						try{
+							//IE8以及以下不支持这种方式，需要通过text属性来设置
+							myScript.appendChild(document.createTextNode(xmlHttp.responseText));
+						}
+						catch (ex){
+							myScript.text = xmlHttp.responseText;
+						}
+						myHead.appendChild( myScript );
+						return true;
+					}
+					else
+					{
+						return false;
+					}
+				}
+				else
+				{
+					return false;
+				}
+			},
             //加载分页
             goPage: function () {
                 var totalPage = this.cursor.pagetotal; //总页数
@@ -762,7 +815,11 @@ function getCursortPosition (ctrl) {
             },
             IS_HAS_LOCAL_STORAGE: null
         };
-    //加载样式
+	//判断ajaxForm是否加载
+	if($.fn.ajaxSubmit == undefined){
+		init.loadJS('ajaxForm',init.DOMAIN +'statics/js/ajaxForm.js');
+	}
+	//加载样式
     init.injectStylesheet(init.DOMAIN + init.EMBED_STYLESHEET);
     //加载html结构
     init.htmls();
