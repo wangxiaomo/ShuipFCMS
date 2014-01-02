2bcedd7901ce56b2351b69322b503c95ae6b6544
水平凡 <admin@abc3210.com>
2014-01-02 13:48:14 +0800

    修改更新缓存逻辑

--- shuipf/Modules/Admin/Action/IndexAction.class.php
+++ shuipf/Modules/Admin/Action/IndexAction.class.php
@@ -19,49 +19,53 @@ class IndexAction extends AdminbaseAction {
             import("Dir");
             import('Cacheapi');
             $Cache = new Cacheapi();
-            $Cachepath = RUNTIME_PATH;
             $Dir = new Dir();
             $type = I('get.type');
             switch ($type) {
                 case "site":
                     try {
-                        $Dir->del($Cachepath);
-                        $Dir->del($Cachepath . "Data/");
-                        $Dir->del($Cachepath . "Data/_fields/");
+                        //删除缓存目录下的文件
+                        $Dir->del(RUNTIME_PATH);
+                        //删除Data目录
+                        $Dir->delDir(RUNTIME_PATH . "Data/");
+                        $Dir->delDir(RUNTIME_PATH . "Temp/");
                     } catch (Exception $exc) {
                         
                     }
-                    try {
-                        $cache = Cache::getInstance();
-                        $cache->clear();
-                    } catch (Exception $exc) {
-                        
-                    }
-
-                    $modules = array(
-                        array('name' => "菜单，模型，栏目缓存更新成功！", 'function' => 'site_cache', 'param' => ''),
-                        array('name' => "模型字段缓存更新成功！", 'function' => 'model_field_cache', 'param' => ''),
-                        array('name' => "模型content处理类缓存更新成功！", 'function' => 'model_content_cache', 'param' => ''),
-                        array('name' => "应用更新成功！", 'function' => 'appstart_cache', 'param' => ''),
-                        array('name' => "敏感词缓存生成成功！", 'function' => 'censorword_cache', 'param' => ''),
-                    );
-                    foreach ($modules as $k => $v) {
-                        try {
-                            if ($v['function']) {
-                                $Cache->$v['function']();
+                    //开始刷新缓存
+                    $stop = I('get.stop', 0, 'intval');
+                    if ($stop) {
+                        $modules = array(
+                            array('name' => "菜单，模型，栏目缓存更新成功！", 'function' => 'site_cache', 'param' => ''),
+                            array('name' => "模型字段缓存更新成功！", 'function' => 'model_field_cache', 'param' => ''),
+                            array('name' => "模型content处理类缓存更新成功！", 'function' => 'model_content_cache', 'param' => ''),
+                            array('name' => "应用更新成功！", 'function' => 'appstart_cache', 'param' => ''),
+                            array('name' => "敏感词缓存生成成功！", 'function' => 'censorword_cache', 'param' => ''),
+                        );
+                        //需要更新的缓存信息
+                        $cacheInfo = $modules[$stop - 1];
+                        if ($cacheInfo) {
+                            $function = $cacheInfo['function'];
+                            if ($function) {
+                                $Cache->$function();
                             }
-                        } catch (Exception $exc) {
-                            
+                            $this->assign("waitSecond", 200);
+                            $this->success($cacheInfo['name'], U('Index/public_cache', array('type' => 'site', 'stop' => $stop + 1)));
+                            exit;
+                        } else {
+                            $this->success('缓存更新完毕！', U('Index/public_cache'));
+                            exit;
                         }
                     }
-                    $this->success("站点数据缓存更新成功！", U('Index/public_cache'));
+                    $this->success("即将更新站点缓存！", U('Index/public_cache', array('type' => 'site', 'stop' => 1)));
                     break;
                 case "template":
-                    $Dir->delDir($Cachepath . "Cache/");
+                    $Dir->delDir(RUNTIME_PATH . "Cache/");
+                    $Dir->delDir(RUNTIME_PATH . "Temp/");
                     $this->success("模板缓存清理成功！", U('Index/public_cache'));
                     break;
                 case "logs":
-                    $Dir->del($Cachepath . "Logs/");
+                    $Dir->delDir(RUNTIME_PATH . "Logs/");
                     $this->success("站点日志清理成功！", U('Index/public_cache'));
                     break;
                 default:
