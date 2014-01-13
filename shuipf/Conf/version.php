b6316a591508ff0ddad7b4d47b7505ada6e18b5c
水平凡 <admin@abc3210.com>
2014-01-13 15:49:07 +0800

    更新版本号到1.4.0

--- shuipf/Conf/version.php
+++ shuipf/Conf/version.php
@@ -7,5 +7,5 @@
 return array(
     "SHUIPF_APPNAME" => "ShuipFCMS_UTF8_Free", //产品名称
     "SHUIPF_BUILD" => 20140113, //产品流水号
-    "SHUIPF_VERSION" => "1.3.3400", //产品版本号
+    "SHUIPF_VERSION" => "1.4.0", //产品版本号
 );
