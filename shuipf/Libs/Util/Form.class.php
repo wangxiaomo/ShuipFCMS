292abb25a2e29e755a0d7e545f89376e1184ce28
水平凡 <admin@abc3210.com>
2014-06-20 13:33:35 +0800

    继续调整编辑器设置

--- shuipf/Libs/Util/Form.class.php
+++ shuipf/Libs/Util/Form.class.php
@@ -65,7 +65,7 @@ class Form {
         $str .= " var editor{$textareaid} = UE.getEditor('{$textareaid}',{  
                             textarea:'" . ( in_array($module, array("Content", "content")) ? "info[$textareaid]" : "$textareaid" ) . "',
                             toolbars:[$toolbar],
-                            autoHeightEnabled:true
+                            autoHeightEnabled:false
                       });
                       editor{$textareaid}.ready(function(){
                             editor{$textareaid}.execCommand('serverparam', {
